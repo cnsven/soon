a0ec1fdd71283112b9b2dc18c9ed372d92dfdbad
Ben Sandofsky <sandofsky@gmail.com>
2015-04-25 14:57:02 -0700

    Initial Checkin.

---
+++ Soon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Soon
+//
+//  Created by Ben Sandofsky on 4/25/15.
+//  Copyright (c) 2015 Chroma Noir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
